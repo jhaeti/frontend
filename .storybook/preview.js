e5f7130f3a85cf66d20442578a24e518a4e27984
Alhassan Tijani <jhaeti@gmail.com>
2022-12-15 10:40:05 +0000

    setup for mui to work with storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,24 @@
+import theme from '../src/config/theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+const withThemeProvider = (Story) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Story />
+        </ThemeProvider>
+    );
+};
+
+export const decorators = [withThemeProvider];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
