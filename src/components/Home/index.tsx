7c7faf60e1a4edad8aaa37236af17f155ad52840
Alhassan Tijani <jhaeti@gmail.com>
2022-12-22 02:19:59 +0000

    Homepage

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+const Home = () => (
+    <Typography align='center' mt={10} variant='h3'>
+        Welcome to the home page
+    </Typography>
+);
+
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,9 @@
-import styled from 'styled-components';
+import Home from '../components/Home';
 
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-`;
+const index = () => (
+    <div>
+        <Home />
+    </div>
+);
 
-const Title = styled.h1`
-    text-align: center;
-    color: palevioletred;
-`;
-const homepage = () => {
-    const str = 'Welcome to my boilerplate app';
-    return (
-        <Wrapper>
-            <Title>{str}</Title>
-        </Wrapper>
-    );
-};
-
-export default homepage;
+export default index;
